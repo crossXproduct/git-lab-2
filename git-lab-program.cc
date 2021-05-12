264d8dbcc81b8af7f52c1f91a9adc7655ddea117
Elise Aaron <ea554820@ohio.edu>
2021-05-12 11:22:20 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Elise Aaron
+ *        Date: 12-05-2021
+ * Description: Test program for git lab.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
